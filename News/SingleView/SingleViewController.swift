cb3836cac1456d08d57c26c184b5c0e50e3bd5ec
Ruslan Latfulin <latfulinr@gmail.com>
2018-12-04 21:22:09 +0300

    add view for selected news

---
+++ News/SingleView/SingleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SingleViewController.swift
+//  News
+//
+//  Created by Ruslan on 01.12.2018.
+//  Copyright © 2018 Ruslan. All rights reserved.
+//
+
+import UIKit
+
+class SingleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
